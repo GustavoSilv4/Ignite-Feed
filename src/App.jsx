e049f913824d2c05d83dec249b619adc70dee538
GustavoSilv4 <GustavoSilv4.contato@gmail.com>
2022-06-27 14:33:28 -0300

    feat: created fakes post

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,41 @@ import styles from './App.module.css';
 
 import './global.css'
 
+// author: { avatar_url: string, name: string, role: string}
+// publisheadAt: Date
+// content: string
+
+const post = [
+  {
+    id: 1,
+    author: {
+      avatar_url: 'https://github.com/gustavosilv4.png',
+      name: 'Gustavo Silva',
+      role: 'Frontend Developer'
+    },
+    content: [
+      { type: 'paragraph', content: 'Fala galeraa 👋' },
+      { type: 'paragraph', content: 'Acabei de subir mais um projeto no meu portifa.É um projeto que fiz no NLW Return, evento da Rocketseat.O nome do projeto é DoctorCare 🚀' },
+      { type: 'link', content: '👉 jane.design / doctorcare' },
+    ],
+    publishedAt: new Date('2022-05-03 20:00:00'),
+  },
+  {
+    id: 2,
+    author: {
+      avatar_url: 'https://github.com/diegog3.png',
+      name: 'Diego Fernandos',
+      role: 'CTO @ Rocketseat'
+    },
+    content: [
+      { type: 'paragraph', content: 'Fala galeraa 👋' },
+      { type: 'paragraph', content: 'Acabei de subir mais um projeto no meu portifa.É um projeto que fiz no NLW Return, evento da Rocketseat.O nome do projeto é DoctorCare 🚀' },
+      { type: 'link', content: '👉 jane.design / doctorcare' },
+    ],
+    publishedAt: new Date('2022-06-23 21:00:00'),
+  },
+]
+
 export function App() {
   return (
     <div>
